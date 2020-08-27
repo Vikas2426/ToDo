3c49fc81c5d75aa382903a03a253ae8de0a5c26f
Vikas Choubey <vikas.cby@gmail.com>
2020-08-27 17:04:58 +0530

    Updates

---
+++ precache-manifest.6738c1f9f92c7fe27867d65da3996408.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea1e197f797087935aeffdd5e0aa687e",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "0a833b378e137c183682",
+    "url": "/ToDo/static/js/2.45e775ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo/static/js/2.45e775ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a227ee8cc565f55178",
+    "url": "/ToDo/static/js/main.dc414b1c.chunk.js"
+  },
+  {
+    "revision": "cf31d88e2af6dda70544",
+    "url": "/ToDo/static/js/runtime-main.75c02b01.js"
+  }
+]);
