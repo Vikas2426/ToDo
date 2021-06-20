b839ec4624a74991c95928109ef88c63adc47569
Vikas592 <vikas.choubey@successive.tech>
2021-06-20 11:22:49 +0530

    feat: add DND

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -7,53 +7,103 @@ import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const ToDoList = props => {
     const [toDoList, setToDoList] = useState([]);
-    let URL = 'https://secret-shelf-76542.herokuapp.com/todos/';
+    const [modifList, setModifList] = useState([]);
 
-    const addToList = async (newContent) => {
-        setToDoList(await fetch(URL, {
+    const URL = 'https://secret-shelf-76542.herokuapp.com/todos/';
+    const headers = {
+        'content-type': 'application/json'
+    }
+
+    const addToDB = async newContent => {
+        const response = await fetch(URL, {
             method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
+            headers,
             body: JSON.stringify({ 'content': newContent, 'dateTime': new Date().toLocaleString() }),
-        }).then(response => response.json()));
+        });
 
     };
-    const removeFromList = async id => {
-        setToDoList(await fetch(URL, {
+
+    const removeFromDB = async id => {
+        const response = await fetch(URL, {
             method: 'DELETE',
-            headers: {
-                'content-type': 'application/json'
-            },
+            headers,
             body: JSON.stringify({ '_id': id }),
-        }).then(response => response.json()));
+        });
     };
+    function onDragEnd(result) {
+        // dropped outside the list
+        if (!result.destination) {
+            return;
+        }
+        const items = reorder([...toDoList], result.source.index, result.destination.index,);
+        setToDoList(items);
+    }
+
+    const addToList = (content) => {
+        setToDoList([...toDoList, { _id: 'tempId' + content, content, dateTime: new Date().toLocaleString() }])
+    }
+
+    const removeFromList = (id) => {
+        setToDoList(toDoList.filter(item => item._id !== id));
+    }
+
+    const updateDB = () => {
+        modifList.forEach(item => removeFromDB(item._id));
+        toDoList.forEach(item => addToDB(item.content));
+    }
 
     useEffect(() => {
         const getToDoList = async () => {
-            setToDoList(await fetch('https://secret-shelf-76542.herokuapp.com/todos/').then(response => response.json()));
+            const response = await fetch(URL);
+            const list = await response.json();
+            setModifList(list);
+            setToDoList(list);
         }
         getToDoList();
+        // window.addEventListener("beforeunload", updateDB);
+
     }, []);
 
     return (
         <div className="todo-input-list">
             <ul className="collection with-header">
                 <li className="collection-header"><h5>To-Dos</h5></li>
-                {toDoList.map((todo, index) =>
-                    <li className="collection-item" key={index} >
-                        <ToDos
-                            // key={index}
-                            number={index}
-                            id={todo._id}
-                            todo={todo.content}
-                            date={todo.dateTime}
-                            remItem={removeFromList}
-                        />
-                    </li>
+                <DragDropContext onDragEnd={onDragEnd}>
+                    <Droppable droppableId="droppable">
+                        {(provided, snapshot) => (
+                            <div
+                                {...provided.droppableProps}
+                                ref={provided.innerRef}
+                            >
+                                {toDoList.map((todo, index) =>
+                                    <Draggable key={todo._id} draggableId={todo._id} index={index}>
+                                        {(provided, snapshot) => (
+                                            <li className="collection-item"
+                                                ref={provided.innerRef}
+                                                {...provided.draggableProps}
+                                            >
+                                                <span {...provided.dragHandleProps} className='drag-icon-container'>
+                                                    <img className='drag-icon' src={require('../make_sequence.png')} alt='drag-icon' />
+                                                </span>
+                                                <ToDos
+                                                    number={index}
+                                                    id={todo._id}
+                                                    todo={todo.content}
+                                                    date={todo.dateTime}
+                                                    remItem={removeFromList}
+                                                />
+                                            </li>
+                                        )}
+                                    </Draggable>
                 )
                 }
-                <li className="collection-item">
+                                {provided.placeholder}
+                            </div>
+                        )}
+                    </Droppable>
+                </DragDropContext>
+
+                <li className="collection-item input-field">
                 <InputField addNewItem={addToList} />
                 </li>
             </ul>
@@ -61,5 +111,13 @@ const ToDoList = props => {
 
     )
 }
+
+
+
+const reorder = (list, startIndex, endIndex) => {
+    const [removed] = list.splice(startIndex, 1);
+    list.splice(endIndex, 0, removed);
+    return list;
+};
 
 export default ToDoList;
--- src/components/ToDos.jsx
+++ src/components/ToDos.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ToDos.css';
 
 
 const ToDos = (props) => {
+
+    const [done, setDone] = useState(false);
     const sendId = () => {
         props.remItem(props.id);
+    }
+
+    const toggleDone = () => {
+        setDone(!done);
     }
     return (
 
         <div id='to-do-items'>
-            <div>
+            <div className='todo-details' onClick={toggleDone} style={{ textDecorationLine: done ? 'line-through' : 'inherit' }}>
                 <span className='todo-item'>{props.todo}</span>
+                <span id='date-time' className='grey-text date-time'>{props.date}</span>
             </div>
 
-            <span>
-                <span  id='date-time' className='grey-text date-time'>{props.date}</span>
-                <i className='secondary-content material-icons red-text delete-btn' onClick={sendId} >
-                    delete
-                        </i>
+            <span className='date-action-container'>
+                <img src={require('../delete-outline.svg')} alt='delete-icon' onClick={sendId} />
             </span>
 
-        </div >
+        </div>
 
     )
 
